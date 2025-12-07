bc1f5bf92e4463d4fc0c1796af520ec3e4ffe31c
Alexskh5 <delapenasamalexies@gmail.com>
2025-12-08 02:02:20 +0800

    update vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    host: true,
+    port: 5173,
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+    historyApiFallback: true
+  },
+});
